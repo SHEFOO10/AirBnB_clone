da76386acbb19bd1309fd13a1249d67bc3e867eb
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-12-11 03:18:09 +0200

    update console

--- console.py
+++ console.py
@@ -19,6 +19,43 @@ class HBNBCommand(cmd.Cmd):
         """ to exit the program """
         return True
 
+    def do_create(self, line):
+        """ create new object from classname inside the line """
+        if line == '':
+            print ('** class name missing **')
+        else:
+            new_instance = self.return_class(line.split()[0].strip('"'))
+            new_instance.save()
+
+    def do_show(self, line):
+        """ Show the object """
+        from models import storage
+        from models.base_model import BaseModel
+        if line == '':
+            print("** class name missing **")
+        arguments = line.split()
+        instance = self.return_class(arguments[0].strip('"'))
+
+        if len(arguments) == 1:
+                (print("** instance id missing **")
+                    if isinstance(instance, BaseModel)
+                    else print("** class doesn't exist **"))
+        elif len(arguments) >= 2:
+            try:
+                print(storage.all()[arguments[0]+'.'+arguments[1]])
+            except KeyError:
+                print("** no instance found **")
+
+    def return_class(self, classname):
+        """ check if class exists and return it """
+        from models.base_model import BaseModel
+        classes = {
+        'BaseModel': BaseModel
+        }
+        try:
+            return classes[classname]()
+        except KeyError:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
