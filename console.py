09437b55e634400c27d3aaee2247a215eabe72cc
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-12-11 05:12:30 +0200

    console.py

--- console.py
+++ console.py
@@ -23,63 +23,82 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, line):
         """ create new object from classname inside the line """
         if line == '':
-            print ('** class name missing **')
+            print('** class name missing **')
         else:
-            new_instance = self.return_class(line.split()[0].strip('"'))
+            new_instance = self.return_class(line.split()[0].strip('"'))()
             new_instance.save()
             print(new_instance.id)
 
     def do_show(self, line):
         """ Show the object """
-        from models.base_model import BaseModel
-        if line == '':
-            print("** class name missing **")
-        arguments = line.split()
-        instance = self.return_class(arguments[0].strip('"'))
-
-        if len(arguments) == 1:
-                (print("** instance id missing **")
-                    if isinstance(instance, BaseModel)
-                    else '')
-        elif len(arguments) >= 2:
-            try:
-                print(storage.all()[arguments[0]+'.'+arguments[1]])
-            except KeyError:
-                print("** no instance found **")
+        key = self.validate_line(line)
+        if key is not None:
+            print(storage.all()[key])
 
     def do_destory(self, line):
         """ destory the object with it's id """
-        from models.base_model import BaseModel
-        if line.strip() == '':
-            print("** class name missing **")
-            return
-        arguments = line.split()
-        instance = self.return_class(arguments[0].strip('"'))
-        if len(arguments) == 1:
-                (print("** instance id missing **")
-                    if isinstance(instance, BaseModel)
-                    else print("** class doesn't exist **"))
-        elif len(arguments) >= 2:
-            try:
-                del storage.all()[arguments[0].strip()+'.'+arguments[1].strip()]
-                storage.save()
-            except KeyError:
-                print("** no instance found **")
+        key = self.validate_line(line)
+        if key is not None:
+            del storage.all()[key]
+            storage.save()
 
     def do_all(self, line):
         """ return all object as list of strings """
-        from models.base_model import BaseModel
+        if line.strip() == '':
+            print([obj.__str__() for obj in storage.all().values()])
+        else:
+            if self.return_class(line.strip()) is not None:
+                print([obj.__str__() for obj in storage.all().values()
+                      if obj.__class__.__name__ == line.strip()])
 
     def return_class(self, classname):
         """ check if class exists and return it """
         from models.base_model import BaseModel
         classes = {
-        'BaseModel': BaseModel
+            'BaseModel': BaseModel
         }
         try:
-            return classes[classname]()
+            return classes[classname]
         except KeyError:
             print("** class doesn't exist **")
+
+    def validate_line(self, line):
+        """ validate arguments """
+        from models.base_model import BaseModel
+        if line.strip() == '':
+            print("** class name missing **")
+            return
+        arguments = line.split()
+        instance = self.return_class(arguments[0].strip('"'))()
+        if len(arguments) == 1:
+            (print("** instance id missing **")
+                if instance is not None
+                else '')
+        elif len(arguments) >= 2:
+            try:
+                self.have_instances(arguments[0], arguments[1])
+                return f'{arguments[0]}.{arguments[1]}'
+            except KeyError:
+                print("** no instance found **")
+
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id """
+        key = self.validate_line(line)
+        if key is not None:
+            arguments = line.split()
+            arguments_len = len(line.split()[2:])
+            if arguments_len == 0:
+                print("** attribute name missing **")
+            elif arguments_len == 1:
+                print("** value missing **")
+            elif arguments_len >= 2:
+                setattr(storage.all()[key], arguments[2].strip('"'),
+                        arguments[2].__class__(arguments[3].strip('"')))
+                storage.save()
+
+    def have_instances(self, classname, obj_id):
+        """ test if class have instances """
+        storage.all()[classname+'.'+obj_id]
 
 
 if __name__ == '__main__':
